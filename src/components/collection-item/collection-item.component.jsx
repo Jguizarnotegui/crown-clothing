b78143c86e6bc95e1e1f520223c47bebe06c265c
Joshua <jguizarnotegui@gmail.com>
2020-12-01 03:30:01 -0800

    added checkout page, checkout item component and updated cart to use selectors from reselect

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { connecct, connect  } from 'react-redux';
+import { connect  } from 'react-redux';
 import CustomButton from '../custom-button/custom-button.component';
 import { addItem } from '../../redux/cart/cart.actions';
 import './collection-item.styles.scss';
